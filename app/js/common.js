5a5318350ff84d326d12b1be7fff071310d3ed20
Danil Kostyn <kostynd1@gmail.com>
2021-01-14 15:24:35 +0200

    Commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+let openModal = document.querySelector('.navigation__item.auth');
+let closeModal = document.querySelector('.modal__close');
+let modal = document.querySelector('.modal');
 
-	// Custom JS
+openModal.onclick = () => {
+	modal.classList.add('active');
 
-});
+	document.body.style.overflow = 'hidden';
+};
+
+closeModal.onclick = () => {
+	modal.classList.remove('active');
+
+	document.body.style.overflow = 'auto';
+};
